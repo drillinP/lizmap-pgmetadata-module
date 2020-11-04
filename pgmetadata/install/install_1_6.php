0e00daf11fc5d836685baaa0e05f66d5b57a0b63
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-11-04 12:46:07 +0100

    Fixing typo

--- pgmetadata/install/install_1_6.php
+++ pgmetadata/install/install_1_6.php
@@ -11,13 +11,5 @@ class pgmetadataModuleInstaller extends jInstallerModule
 {
     public function install()
     {
-        //if ($this->firstDbExec())
-        //    $this->execSQLScript('sql/install');
-
-        /*if ($this->firstExec('acl2')) {
-            jAcl2DbManager::addSubject('my.subject', 'adresse~acl.my.subject', 'subject.group.id');
-            jAcl2DbManager::addRight('admins', 'my.subject'); // for admin group
-        }
-        */
     }
 }
