87b0f2e1594fdf1da161890e5fdbeeccb9e5dc68
Drillin Pierre <sio.drillin@gmail.com>
2020-11-04 17:26:16 +0100

    Adding html in subdock and show or hide with button

--- pgmetadata/www/pgmetadata.js
+++ pgmetadata/www/pgmetadata.js
@@ -40,9 +40,45 @@ var lizPgmetadata = function() {
         );
     }
 
+    function hideFunction() {
+        var x = document.getElementById("pgmetadata-content");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+    }
+
     function set_metadata_in_subdock(html) {
         if (html) {
             console.log('html has content: need to fill in subdock Metadata content with HTML');
+            // Add html content in div
+            html = '<div id="pgmetadata-content">'+ html +'</div>';
+
+            // Get subdock
+            var subdock = $('.sub-metadata');
+            content = subdock.find('div[class="menu-content"]');
+
+            // create button
+            var btn = $('<button onClick="hideFunction()">Fiche détails</button>');
+            btn.addClass("btn btn-mini pgmetadata-button");
+
+            // Add button and html to subdock
+            content.append(btn);
+            content.append(html);
+
+            // get and hide content
+            var x = document.getElementById("pgmetadata-content");
+            x.style.display = "none";
+
+            // adding showing / hiding on click
+            btn.click(function(){
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            });
         } else {
             console.log('html is null: need to erase subdock Metadata content');
         }
