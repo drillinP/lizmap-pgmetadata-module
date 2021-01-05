9ed9c602d6cdd734aded5f2e8f8485853490d85f
Drillin Pierre <sio.drillin@gmail.com>
2021-01-05 09:55:30 +0100

    code refactoring

--- pgmetadata/classes/search.class.php
+++ pgmetadata/classes/search.class.php
@@ -13,6 +13,7 @@ class search
         'check_dataset' => "SELECT tablename FROM pg_tables WHERE schemaname = 'pgmetadata' AND tablename = 'dataset'",
         'get_html' => 'SELECT pgmetadata.get_dataset_item_html_content($1, $2, $3) AS html',
         'get_html_default' => 'SELECT pgmetadata.get_dataset_item_html_content($1, $2) AS html',
+        'get_version' => "SELECT column_name FROM information_schema.columns WHERE table_schema = 'pgmetadata' AND table_name = 'glossary' AND column_name LIKE 'label_%';",
     );
 
     protected function getSql($option)
@@ -52,6 +53,12 @@ class search
             return null;
         }
 
-        return $this->query($sql, $filterParams, $profile);
+        try {
+            $result = $this->query($sql, $filterParams, $profile);
+        } catch (Exception $e) {
+            return array('status' => 'error', 'message' => 'Error at the query concerning '.$option);
+        }
+
+        return $result->fetchAll();
     }
 }
